05c0c29883c8a036378cff2dd7fc7a6e63883611
Augusto Guimaraes <augusto.guimaraes@ibm.com>
2026-03-15 13:09:42 -0300

    atualização os testes com os novos paths  imports

---
+++ tests/pages/index.ts
@@ -0,0 +1,6 @@
+export { BasePage } from "./BasePage";
+export { LoginPage } from "./auth/LoginPage";
+export { SidebarComponent } from "./components/SidebarComponent";
+export { FlowEditorPage } from "./flows/FlowEditorPage";
+export { PlaygroundPage } from "./flows/PlaygroundPage";
+export { MainPage } from "./main/MainPage";
